aedab348b17cb255d24906fa5cb6820cd60a61e4
vvikot <rymsha.viktor@gmail.com>
2020-02-12 17:22:34 +0300

    fix: delete id and href

--- src/static/js/smoothScroll.js
+++ src/static/js/smoothScroll.js
@@ -2,7 +2,7 @@ if($('.top-description-text-more')){
   $(document).click(function(event) {
     if($(event.target).is($('.top-description-text-more'))){
       $('html,body').stop().animate({ scrollTop: $('.bottom-page-description').offset().top }, 1000);
-      e.preventDefault();
+      event.preventDefault();
     }
   })
 }
