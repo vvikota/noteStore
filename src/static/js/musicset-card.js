08a921cb567505934e47045f1f1b6d2a63eb762c
vvikot <rymsha.viktor@gmail.com>
2020-02-20 10:48:06 +0300

    finish musicset card

--- src/static/js/musicset-card.js
+++ src/static/js/musicset-card.js
@@ -1,7 +1,8 @@
 var relatedSlider = $('.related-slider');
 var recentlySlider = $('.recently-slider');
 
-$(document).ready(function() {
+$(document).ready(function () {
+
   if (relatedSlider.length) {
     relatedSlider.slick({
       dots: true,
@@ -10,14 +11,23 @@ $(document).ready(function() {
       variableWidth: true
     });
     noteSliderSpotter(185);
-   
-  } if(recentlySlider.length) {
+
+  }
+  if (recentlySlider.length) {
     recentlySlider.slick({
-        dots: true,
-        slidesToScroll: 1,
-        infinite: true,
-        variableWidth: true
-      });
-      noteSliderSpotter(185);
-    }
+      dots: true,
+      slidesToScroll: 1,
+      infinite: true,
+      variableWidth: true
+    });
+    noteSliderSpotter(185);
+  }
+  if ($('.radio-group input').length) {
+    $('.radio-group input').change(function (event) {
+      var oldPrice = $(event.target).parent().parent().find('.input-old-price').text().slice(0, -2);
+      var newPrice = $(event.target).parent().parent().find('.input-new-price').text().slice(0, -2);
+      var profit = oldPrice - newPrice;
+      $('.order-form-profit').text('Ваша выгода: ' + profit + ' р.')
+    })
+  }
 })
--- src/static/js/product-card.js
+++ src/static/js/product-card.js
@@ -1,9 +1,9 @@
-$(document).click(function (event) {
-  var button = $('.js-order-product'); 
+// $(document).click(function (event) {
+//   var button = $('.js-order-product'); 
 
 
-  if (button.is(event.target)) {
-    event.preventDefault();
-    button.addClass('js-link-to-busket');
-  }
-}
+//   if (button.is(event.target)) {
+//     event.preventDefault();
+//     button.addClass('js-link-to-busket');
+//   }
+// }
