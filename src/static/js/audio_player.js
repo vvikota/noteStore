0937e1eb7a8c06825b020878083e1c7850db1d89
vvikot <rymsha.viktor@gmail.com>
2020-02-21 13:00:57 +0300

    finish player

--- src/static/js/audio_player.js
+++ src/static/js/audio_player.js
@@ -3,15 +3,27 @@ var track = $('.track')[0];
 var accumTime = $('.time-accum');
 var residueTime = $('.time-residue');
 var duration = track.duration;
+var progressBar = $('.progress__filled')[0];
+var toggleVolume  = $('.toggle-volume');
 
 track.onloadedmetadata = function () {
-
   duration = Math.round(track.duration);
 
   function showTimer() {
 
     function transformTime(time, timePlace) {
-      var transformedTime = Math.floor(time/60) + ':' + (time % 60);
+      var minutes = Math.floor(time / 60);
+      var seconds = (time % 60);
+
+      if (minutes < 10) {
+        minutes = '0' + minutes;
+      };
+
+      if (seconds < 10) {
+        seconds = '0' + seconds;
+      }
+
+      var transformedTime = minutes + ':' + seconds;
       timePlace.text(transformedTime)
     }
 
@@ -20,7 +32,12 @@ track.onloadedmetadata = function () {
 
     var durationTrack = duration - currentTime;
     transformTime(durationTrack, residueTime);
-  }
+
+    var percent = (currentTime / durationTrack) * 100;
+    progressBar.style.width = "".concat(percent, "%");
+  };
+
+  showTimer();
 
   var autoPlay = setInterval(showTimer, 1000);
   clearInterval(autoPlay);
@@ -28,7 +45,6 @@ track.onloadedmetadata = function () {
   $(document).click(function (event) {
 
     if (playButton.is(event.target)) {
-
       playButton.toggleClass('pause');
 
       var method = track.paused ? 'play' : 'pause';
@@ -36,7 +52,16 @@ track.onloadedmetadata = function () {
 
       method === 'play' ? (autoPlay = setInterval(showTimer, 1000)) :
         clearInterval(autoPlay);
+    } else if (toggleVolume.is(event.target)){
+      if(toggleVolume.hasClass('volume-off')){
+        toggleVolume.removeClass('volume-off');
+        track.volume = 1;
+        console.log(track.volume);
+      } else {
+        toggleVolume.addClass('volume-off');
+        track.volume = 0;
+        console.log(track.volume);
+      }
     }
   })
-
 };
