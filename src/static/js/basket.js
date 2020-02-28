5855d041c0f9b23e09f9e80e3c15800fe4ceca78
vvikot <rymsha.viktor@gmail.com>
2020-02-28 15:51:39 +0300

    feat: add calculaor functional to basket Page

--- src/static/js/basket.js
+++ src/static/js/basket.js
@@ -1,25 +1,20 @@
 // -------- BASKET page
+$(document).ready(function () {
+  if ($('.basket-main__form').length > 0) {
+    var mail = $('#mail');
+    var checkbox = $('.order__form-checkbox-input');
+    var submitButton = $('.basket-main__agreement-button');
 
-if ($('.basket-main__form').length > 0) {
-  var mail = $('#mail');
-  var checkbox = $('.order__form-checkbox-input');
-  var submitButton = $('.basket-main__agreement-button');
-
-  $(document).ready(function () {
     $('.basket-main__form').on('input', function (event) {
-
       function validateOrder() {
-        
-        if(mail.hasClass('correct') && checkbox.hasClass('correct')){
+        if (mail.hasClass('correct') && checkbox.hasClass('correct')) {
           submitButton.addClass('order-ready');
         } else {
           submitButton.removeClass('order-ready')
         }
-      }
-
+      };
 
       if (mail.is(event.target)) {
-
         var regExp = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
         var inputValue = mail.val();
 
@@ -37,13 +32,40 @@ if ($('.basket-main__form').length > 0) {
         }
         validateOrder();
       }
-
-
-
-
     })
-  })
-}
+  }
+
+  if ($('.delete-product').length > 0) {
+
+    function calculator() {
+      var amountContainer = $('.basket-main__score-amount');
+      var sumContainer = $('.basket-main__score-sum');
+      var prices = $('.js-product-price');
+      var amount = 0;
+      var sum = 0;
+
+      prices.each(function (item) {
+        var price = $(this).text().slice(0, -3).replace(/\s+/g, '');
+        amount = item + 1;
+        sum = sum + (+price);
+      })
+      sum = sum.toString().replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+
+      amountContainer.text('Товаров (' + amount + ')');
+      sumContainer.text('На сумму: ' + sum + 'р.')
+    };
+
+    calculator();
+
+    $(document).click(function (event) {
+      if ($('.delete-product').is(event.target)) {
+        $(event.target).parent().parent().remove();
+        calculator()
+      }
+    })
+  }
+})
+
 
 
 // -------- end BASKET page
