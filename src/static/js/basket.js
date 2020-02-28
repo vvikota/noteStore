f90cf5488949aa9a647a7f938366ff272fcb04f2
vvikot <rymsha.viktor@gmail.com>
2020-02-28 16:49:45 +0300

    finish: desktop basket Page

--- src/static/js/basket.js
+++ src/static/js/basket.js
@@ -1,19 +1,21 @@
 // -------- BASKET page
-$(document).ready(function () {
-  if ($('.basket-main__form').length > 0) {
-    var mail = $('#mail');
-    var checkbox = $('.order__form-checkbox-input');
-    var submitButton = $('.basket-main__agreement-button');
+//$(document).ready(function () {
+if ($('.basket-main__form').length > 0) {
+  var mail = $('#mail');
+  var checkbox = $('.order__form-checkbox-input');
+  var submitButton = $('.basket-main__agreement-button');
 
-    $('.basket-main__form').on('input', function (event) {
-      function validateOrder() {
-        if (mail.hasClass('correct') && checkbox.hasClass('correct')) {
-          submitButton.addClass('order-ready');
-        } else {
-          submitButton.removeClass('order-ready')
-        }
-      };
+  $('.basket-main__form').on('input', function (event) {
 
+    function validateOrder() {
+      if (mail.hasClass('correct') && checkbox.hasClass('correct')) {
+        submitButton.addClass('order-ready');
+      } else {
+        submitButton.removeClass('order-ready')
+      }
+    };
+
+    $(document).ready(function () {
       if (mail.is(event.target)) {
         var regExp = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
         var inputValue = mail.val();
@@ -33,38 +35,40 @@ $(document).ready(function () {
         validateOrder();
       }
     })
-  }
+  })
+}
 
-  if ($('.delete-product').length > 0) {
+if ($('.delete-product').length > 0) {
 
-    function calculator() {
-      var amountContainer = $('.basket-main__score-amount');
-      var sumContainer = $('.basket-main__score-sum');
-      var prices = $('.js-product-price');
-      var amount = 0;
-      var sum = 0;
+  function calculator() {
+    var amountContainer = $('.basket-main__score-amount');
+    var sumContainer = $('.basket-main__score-sum');
+    var prices = $('.js-product-price');
+    var amount = 0;
+    var sum = 0;
 
-      prices.each(function (item) {
-        var price = $(this).text().slice(0, -3).replace(/\s+/g, '');
-        amount = item + 1;
-        sum = sum + (+price);
-      })
-      sum = sum.toString().replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+    prices.each(function (item) {
+      var price = $(this).text().slice(0, -3).replace(/\s+/g, '');
+      amount = item + 1;
+      sum = sum + (+price);
+    })
+    sum = sum.toString().replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
 
-      amountContainer.text('Товаров (' + amount + ')');
-      sumContainer.text('На сумму: ' + sum + 'р.')
-    };
+    amountContainer.text('Товаров (' + amount + ')');
+    sumContainer.text('На сумму: ' + sum + ' р.')
+  };
 
-    calculator();
-
+  calculator();
+  $(document).ready(function () {
     $(document).click(function (event) {
       if ($('.delete-product').is(event.target)) {
         $(event.target).parent().parent().remove();
         calculator()
       }
     })
-  }
-})
+  })
+}
+
 
 
 
